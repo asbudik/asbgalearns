cfd1add9ee7a6b7d2e3b496cf9d2687e6382ce81
Adam Budik <adam@Adams-MacBook-Air.local>
2018-11-17 19:12:44 -0800

    Fix how S3 client gets initialized

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,9 @@
-AWS.config(
-  :access_key_id => ENV["AWS_KEY"],
-  :secret_access_key => ENV["AWS_SECRET"])
+require 'aws-sdk-s3'
 
-S3_CLIENT = AWS::S3.new
+AWS_REGION = 'us-west-1'
+
+S3_CLIENT = Aws::S3::Client.new(
+  :region => AWS_REGION,
+  :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+  :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+)
